c5e4361797978dc037bdee6ba13856688af9ffc1
sakshisangaonkar <115221865+sakshisangaonkar@users.noreply.github.com>
2025-05-29 20:16:05 +0530

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -10,11 +10,11 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
-if n%2 !=0:
+if(n%2 !=0):
     print("Weird")
-elif n%2 ==0 and 2<n<5:
+elif(n>2 and n<=5):
     print("Not Weird")
-elif n%2==0 and 6<n<20:
+elif(n>6 and n<=20):
     print("Weird")
-elif n%2==0 and n>20:
+else:
     print("Not Weird")
